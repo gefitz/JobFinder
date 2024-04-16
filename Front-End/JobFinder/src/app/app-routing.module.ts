ca3ba0d8d5366deb2b6b03a7fa4557d80066ea36
Geovane <almeidafitz@gmail.com>
2024-04-16 19:09:33 -0300

    finalizado tela de cadastro

--- Front-End/JobFinder/src/app/app-routing.module.ts
+++ Front-End/JobFinder/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'cadastro', component: CadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
