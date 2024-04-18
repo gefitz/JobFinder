d29a3b36f41e42acd69de4218803536580dcd021
Geovane <almeidafitz@gmail.com>
2024-04-18 14:07:14 -0300

    commit API

---
+++ Back-End/JobFinder.Data/DataBase/CandidatoDB.cs
@@ -0,0 +1,28 @@
+﻿using JobFinder.Data.DataBase.Sql;
+using JobFinder.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobFinder.Data.DataBase
+{
+    public class CandidatoDB
+    {
+        private readonly Command _command;
+
+        public CandidatoDB(Command command)
+        {
+            _command = command;
+        }
+        public async Task<bool> CandidatoPost(CandidatoInsertModel candidato)
+        {
+            if(candidato == null) return false;
+
+            if(await _command.ExecuteInsert("tbl_Candidato", candidato)) return true;
+
+            return false;
+        }
+    }
+}
---
+++ Back-End/JobFinder.Data/Model/CandidatoModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobFinder.Data.Model
+{
+    public class CandidatoModel
+    {
+        public int? idCandidato { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string CEP { get; set; }
+        public string Rua { get; set; }
+        public int idCidade { get; set; }
+    }
+    public class CandidatoInsertModel
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string CPF { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string CEP { get; set; }
+        public string Rua { get; set; }
+        public int idCidade { get; set; }
+    }
+
+}
